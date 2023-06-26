90e242e5960c0de5031674ea53675ff0ab362a61
LouisCassany <louis.cassany@beyond-the-sea.fr>
2023-06-26 16:47:05 +0200

    Initial commit

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex justify-center flex-col h-screen">
+    <div class="text-5xl text-red-800 font-mono">Pathbuilder</div>
+    <textarea class="border-primary border-2 rounded-md w-1/2 h-1/2"></textarea>
+  </div>
+</template>
+
+<style scoped>
+textarea {
+  resize: none;
+  outline: none;
+}
+</style>
